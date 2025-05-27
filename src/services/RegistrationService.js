0b900da507e0792ab7e0a6df8861a7a64fe3056b
Silver <silver@silver.com>
2025-05-27 10:01:51 +0300

    ToDo: Groomer Registre rohkem välju

--- src/services/RegistrationService.js
+++ src/services/RegistrationService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
     registrationCustomer(username, password) {
-        return axios.post('http://localhost:8080/register-customer', {
+        return axios.post('/register-customer', {
             username: username,
             password: password,
             roleId: 3
@@ -10,7 +10,7 @@ export default {
     },
 
     registrationGroomer(username, password) {
-        return axios.post('http://localhost:8080/register-groomer', {
+        return axios.post('/register-groomer', {
             username: username,
             password: password,
             roleId: 2
